7eea2355407d855f794cde5c3107ed3eb4fea440
Bence Tóth <bencike04201@gmail.com>
2024-10-03 16:15:12 +0200

    Minor changes in the Blink Test
    Minor changes in the Blink Test

--- SPROJ3G2_Car.c
+++ SPROJ3G2_Car.c
@@ -1,3 +1,17 @@
+/**
+ * Title: Semester Project 3 Group 2 - Car code
+ * Institution: University of Southern Denmark (SDU)
+ * Campus: Sonderborg
+ * File: SPROJ3G2_Car.c
+ * Author: Bence Toth
+ * Date: 03/10/2024
+ * Course: BEng in Electronics
+ * Semester: 3rd
+ * Platform: RP2040
+ * RF module: nRF24L01+
+ * RF Library: https://github.com/andyrids/pico-nrf24
+ */
+
 // Include necessary libraries
 #include <stdio.h>
 #include "pico/stdlib.h"
@@ -12,17 +26,16 @@
 //#include "pico/multicore.h"
 
 #define FREEZE 100
-#define SPI_BAUDRATE 4000000
-#define RF_CHANNEL 110
+#define SPI_BAUDRATE 4000000    // 4 MHz
+#define RF_CHANNEL 110          // 2.51 GHz
 #define THRESHOLD 2000
-//#define ADDRESS_WIDTH 5
-
-//const uint8_t address [ ADDRESS_WIDTH ] = { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;
 
 int main ( ) {
 
+    //const uint8_t address [ ] = { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;
     uint8_t pipe_number = 0 , led_state = 0 ;
     uint16_t payload ;
+
     // initialize all present standard stdio types
     stdio_init_all ( ) ;
 
@@ -40,22 +53,22 @@ int main ( ) {
 
     nrf_manager_t RF_Config = {
         // RF Channel 
-        .channel = RF_CHANNEL,
+        .channel = RF_CHANNEL ,
 
         // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
-        .address_width = AW_5_BYTES,
+        .address_width = AW_5_BYTES ,
 
         // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-        .dyn_payloads = DYNPD_DISABLE,
+        .dyn_payloads = DYNPD_DISABLE ,
 
         // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
-        .data_rate = RF_DR_250KBPS,
+        .data_rate = RF_DR_2MBPS ,
 
         // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-        .power = RF_PWR_NEG_18DBM,
+        .power = RF_PWR_0DBM ,
 
         // retransmission count: ARC_NONE...ARC_15RT
-        .retr_count = ARC_NONE,
+        .retr_count = ARC_NONE ,
 
         // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
         .retr_delay = ARD_250US 
@@ -71,7 +84,7 @@ int main ( ) {
 
     RF24.rx_destination ( DATA_PIPE_0 , ( const uint8_t [ ] ) { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ) ;
 
-    RF24.payload_size ( ALL_DATA_PIPES , sizeof ( payload ) ) ;
+    RF24.payload_size ( DATA_PIPE_0 , sizeof ( payload ) ) ;
 
     RF24.dyn_payloads_disable ( ) ; // Dynamic payloads disabled
 
--- SPROJ3G2_Controller.c
+++ SPROJ3G2_Controller.c
@@ -1,3 +1,17 @@
+/**
+ * Title: Semester Project 3 Group 2 - Controller code
+ * Institution: University of Southern Denmark (SDU)
+ * Campus: Sonderborg
+ * File: SPROJ3G2_Controller.c
+ * Author: Bence Toth
+ * Date: 03/10/2024
+ * Course: BEng in Electronics
+ * Semester: 3rd
+ * Platform: RP2040
+ * RF module: nRF24L01+
+ * RF Library: https://github.com/andyrids/pico-nrf24
+ */
+
 // Include necessary libraries
 #include <stdio.h>
 #include "pico/stdlib.h"
@@ -14,11 +28,8 @@
 #define ADC_PIN 26
 #define ADC_INPUT 0
 #define FREEZE 100
-#define SPI_BAUDRATE 4000000
-#define RF_CHANNEL 110
-#define ADDRESS_WIDTH 5
-
-//const uint8_t address [ ADDRESS_WIDTH ] = { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;
+#define SPI_BAUDRATE 4000000    // 4 MHz
+#define RF_CHANNEL 110          // 2.51 GHz
 
 void ADC_Setup ( uint8_t pin , uint8_t input ) {
     adc_init ( ) ;
@@ -27,6 +38,8 @@ void ADC_Setup ( uint8_t pin , uint8_t input ) {
 }
 
 int main ( ) {
+
+    //const uint8_t address [ ] = { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;
     uint16_t payload ;
     
     stdio_init_all ( ) ;
@@ -46,19 +59,19 @@ int main ( ) {
         .channel = RF_CHANNEL ,
 
         // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
-        .address_width = AW_5_BYTES,
+        .address_width = AW_5_BYTES ,
 
         // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-        .dyn_payloads = DYNPD_DISABLE,
+        .dyn_payloads = DYNPD_DISABLE ,
 
         // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
-        .data_rate = RF_DR_250KBPS,
+        .data_rate = RF_DR_2MBPS ,
 
         // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-        .power = RF_PWR_NEG_18DBM,
+        .power = RF_PWR_0DBM ,
 
         // retransmission count: ARC_NONE...ARC_15RT
-        .retr_count = ARC_NONE,
+        .retr_count = ARC_NONE ,
 
         // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
         .retr_delay = ARD_250US 
