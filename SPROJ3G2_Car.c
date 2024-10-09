4179a9674e5cc95b53cc4234981a96604a07a523
Bence Tóth <bencike04201@gmail.com>
2024-10-09 17:07:59 +0200

    Rewrote functions into separate header file
    Rewrote functions into separate header file

--- SPROJ3G2_Car.c
+++ SPROJ3G2_Car.c
@@ -18,39 +18,55 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
+#include "hardware/spi.h"
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
-#include "nrf24l01/nrf24_driver.h"
-#include "servo/rc.h"
-#include "hardware/spi.h"
+#include "nrf24_driver.h"
+#include "functions.h"
+#include "servo.h"
 //#include "hardware/i2c.h"
 //#include "hardware/timer.h"
 //#include "hardware/uart.h"
 //#include "hardware/irq.h"
 //#include "pico/multicore.h"
 
-#define FREEZE 20
-#define SPI_BAUDRATE 4000000    // 4 MHz SPI baudrate
-#define RF_CHANNEL 110          // 2.51 GHz ISM frequency band
-#define SERVO_PIN  8
-#define INIT_ANGLE 90
-#define MAX_ANGLE 180
-#define MIN_ANGLE 0
-#define ADC_MIN 0
-#define ADC_MAX 4096
-#define THRESHOLD ( ADC_MAX / 2 )
+uint8_t rx_pipe = 0 ;
+payload_t Payload = { 1 , INIT_ANGLE } ;
+nrf_client_t RF24 ;
 
-typedef struct {
-    uint8_t direction ;
-    uint8_t servo_angle ;
-} payload_t ;
+// GPIO pin numbers for nRF24L01
+pin_manager_t RF_Pins = { 
+    .sck = 2,   // Serial Clock
+    .copi = 3,  // Master Out - Slave In
+    .cipo = 4,  // Master In - Slave Out
+    .csn = 5,   // Chip Select Not
+    .ce = 6     // Chip Enable
+} ;
+
+nrf_manager_t RF_Config = {
+    // RF Channel 
+    .channel = RF_CHANNEL ,
+
+    // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
+    .address_width = AW_5_BYTES ,
+
+    // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
+    .dyn_payloads = DYNPD_DISABLE ,
+
+    // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
+    .data_rate = RF_DR_2MBPS ,
+
+    // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
+    .power = RF_PWR_0DBM ,
+
+    // retransmission count: ARC_NONE...ARC_15RT
+    .retr_count = ARC_NONE ,
+
+    // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
+    .retr_delay = ARD_250US 
+} ;
 
 int main ( void ) {
-
-    //const uint8_t address [ ] = { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;
-
-    uint8_t pipe_number = 0 ;
-    payload_t Payload = { 1 , INIT_ANGLE } ;
 
     stdio_init_all ( ) ;    // Initialize all present standard stdio types
 
@@ -60,57 +76,11 @@ int main ( void ) {
     rc_servo Servo = rc_servo_init ( SERVO_PIN ) ;
     rc_servo_start ( &Servo , INIT_ANGLE ) ;
 
-    // GPIO pin numbers for nRF24L01
-    pin_manager_t RF_Pins = { 
-        .sck = 2,   // Serial Clock
-        .copi = 3,  // Master Out - Slave In
-        .cipo = 4,  // Master In - Slave Out
-        .csn = 5,   // Chip Select Not
-        .ce = 6     // Chip Enable
-    } ;
-
-    nrf_manager_t RF_Config = {
-        // RF Channel 
-        .channel = RF_CHANNEL ,
-
-        // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
-        .address_width = AW_5_BYTES ,
-
-        // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-        .dyn_payloads = DYNPD_DISABLE ,
-
-        // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
-        .data_rate = RF_DR_2MBPS ,
-
-        // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-        .power = RF_PWR_0DBM ,
-
-        // retransmission count: ARC_NONE...ARC_15RT
-        .retr_count = ARC_NONE ,
-
-        // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
-        .retr_delay = ARD_250US 
-    } ;
-
-    nrf_client_t RF24 ;
-
-    nrf_driver_create_client ( &RF24 ) ;            // Initialise the RF module
-
-    RF24.configure ( &RF_Pins , SPI_BAUDRATE ) ;    // Configure GPIO pins and SPI
- 
-    RF24.initialise ( &RF_Config ) ;
-
-    RF24.rx_destination ( DATA_PIPE_0 , ( const uint8_t [ ] ) { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ) ;
-
-    RF24.payload_size ( DATA_PIPE_0 , sizeof ( Payload ) ) ;
-
-    RF24.dyn_payloads_disable ( ) ; // Dynamic payloads disabled
-
-    RF24.receiver_mode ( ) ;        // RF module set to RX Mode
+    nRF24_Setup ( &RF24 , &RF_Pins , &RF_Config , SPI_BAUDRATE , sizeof ( payload_t ) , RF24_RX , DYNPD_DISABLE , RF_ADDRESS , DATA_PIPE_0 ) ;
 
     while ( 1 ) {
-        if ( RF24.is_packet ( &pipe_number ) ) {
-            RF24.read_packet ( &Payload , sizeof ( Payload ) ) ;
+        if ( RF24.is_packet ( &rx_pipe ) ) {
+            RF24.read_packet ( &Payload , sizeof ( payload_t ) ) ;
 
             gpio_put ( PICO_DEFAULT_LED_PIN , Payload.direction ) ;
             rc_servo_set_angle ( &Servo , ( uint32_t ) Payload.servo_angle ) ;
--- SPROJ3G2_Controller.c
+++ SPROJ3G2_Controller.c
@@ -17,110 +17,68 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
-#include "nrf24l01/nrf24_driver.h"
-//#include "hardware/spi.h"
+#include "hardware/spi.h"
+#include "nrf24_driver.h"
+#include "functions.h"
 //#include "hardware/i2c.h"
 //#include "hardware/timer.h"
 //#include "hardware/uart.h"
 //#include "hardware/irq.h"
 //#include "pico/multicore.h"
 
-#define POT_X 26
-#define POT_X_INPUT 0
-#define FREEZE 20
-#define INIT_ANGLE 90
-#define MAX_ANGLE 180
-#define MIN_ANGLE 0
-#define ADC_MIN 0
-#define ADC_MAX 4096
-#define TOLERANCE 50
-#define THRESHOLD ( ADC_MAX / 2 )
-#define LOW_LIMIT ( THRESHOLD - TOLERANCE )
-#define HIGH_LIMIT ( THRESHOLD + TOLERANCE )
-#define SPI_BAUDRATE 4000000    // 4 MHz SPI baudrate
-#define RF_CHANNEL 110          // 2.51 GHz ISM frequency band
+uint8_t ADC_Pins [ ] = { 26 , 27 } ;
+uint16_t Pot_X_Val ;
+payload_t Payload = { 1 , INIT_ANGLE } ;
+nrf_client_t RF24 ;
+fn_status_t success ;    // Result of packet transmission
 
-typedef struct {
-    uint8_t direction ;
-    uint8_t servo_angle ;
-} payload_t ;
+// GPIO pin numbers for nRF24L01
+pin_manager_t RF_Pins = { 
+    .sck = 2,   // Serial Clock
+    .copi = 3,  // Master Out - Slave In
+    .cipo = 4,  // Master In - Slave Out
+    .csn = 5,   // Chip Select Not
+    .ce = 6     // Chip Enable
+} ;
 
-int32_t convertInterval ( int32_t x , int32_t in_min , int32_t in_max , int32_t out_min , int32_t out_max ) {
-  return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min ;
-}
+nrf_manager_t RF_Config = {
+    // RF Channel 
+    .channel = RF_CHANNEL ,
 
-void ADC_Setup ( uint8_t pin , uint8_t input ) {
-    adc_init ( ) ;
-    adc_gpio_init ( pin ) ;             // Make sure GPIO is high-impedance, no pullups etc...
-    adc_select_input ( input ) ;        // Select ADC input
-}
+    // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
+    .address_width = AW_5_BYTES ,
+
+    // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
+    .dyn_payloads = DYNPD_DISABLE ,
+
+    // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
+    .data_rate = RF_DR_2MBPS ,
+
+    // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
+    .power = RF_PWR_0DBM ,
+
+    // retransmission count: ARC_NONE...ARC_15RT
+    .retr_count = ARC_NONE ,
+
+    // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
+    .retr_delay = ARD_250US 
+} ;
 
 int main ( void ) {
 
-    //const uint8_t address [ ] = { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;
-    uint16_t adc_val ;
-    payload_t Payload = { 1 , INIT_ANGLE } ;
-    
     stdio_init_all ( ) ;
-    ADC_Setup ( POT_X , POT_X_INPUT ) ;
 
-    // GPIO pin numbers for nRF24L01
-    pin_manager_t RF_Pins = { 
-        .sck = 2,   // Serial Clock
-        .copi = 3,  // Master Out - Slave In
-        .cipo = 4,  // Master In - Slave Out
-        .csn = 5,   // Chip Select Not
-        .ce = 6     // Chip Enable
-    } ;
+    uint8_t adc_setup = ADC_Setup ( ADC_Pins , sizeof ( ADC_Pins ) ) ;
+    hard_assert ( adc_setup == 1 ) ;
 
-    nrf_manager_t RF_Config = {
-        // RF Channel 
-        .channel = RF_CHANNEL ,
-
-        // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
-        .address_width = AW_5_BYTES ,
-
-        // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-        .dyn_payloads = DYNPD_DISABLE ,
-
-        // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
-        .data_rate = RF_DR_2MBPS ,
-
-        // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-        .power = RF_PWR_0DBM ,
-
-        // retransmission count: ARC_NONE...ARC_15RT
-        .retr_count = ARC_NONE ,
-
-        // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
-        .retr_delay = ARD_250US 
-    } ;
-
-    nrf_client_t RF24 ;
-
-    nrf_driver_create_client ( &RF24 ) ;
-
-    RF24.configure ( &RF_Pins , SPI_BAUDRATE ) ;
-
-    RF24.initialise ( &RF_Config ) ;
-
-    RF24.payload_size ( DATA_PIPE_0 , sizeof ( Payload ) ) ;
-
-    RF24.dyn_payloads_disable ( ) ; // Dynamic payloads disabled
-
-    RF24.standby_mode ( ) ;     // TX mode
-
-    fn_status_t success ;    // Result of packet transmission
-
-    // send to receiver's selected pipe address
-    RF24.tx_destination ( ( const uint8_t [ ] ) { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ) ;
+    nRF24_Setup ( &RF24 , &RF_Pins , &RF_Config , SPI_BAUDRATE , sizeof ( payload_t ) , RF24_TX , DYNPD_DISABLE , RF_ADDRESS , DATA_PIPE_0 ) ;
 
     while ( 1 ) {
-        adc_val = adc_read ( ) ;
+        Pot_X_Val = read_ADC ( POT_X_PIN ) ;
 
-        if ( adc_val >= HIGH_LIMIT || adc_val <= LOW_LIMIT ) {
-            Payload.servo_angle = ( uint8_t ) convertInterval ( ( int32_t ) adc_val , ADC_MIN , ADC_MAX , MIN_ANGLE , MAX_ANGLE ) ;
-            Payload.direction = ( adc_val > THRESHOLD ) ? 1 : 0 ;
+        if ( Pot_X_Val >= HIGH_LIMIT || Pot_X_Val <= LOW_LIMIT ) {
+            Payload.servo_angle = ( uint8_t ) convertInterval ( ( int32_t ) Pot_X_Val , ADC_MIN , ADC_MAX , MIN_ANGLE , MAX_ANGLE ) ;
+            Payload.direction = ( Pot_X_Val > THRESHOLD ) ? 1 : 0 ;
         }
 
         else {
@@ -128,11 +86,12 @@ int main ( void ) {
             Payload.direction = 1 ;
         }
 
-        //printf ( "ADC: %d\n" , adc_val ) ;
+        printf ( "ADC: %u <-> Angle: %u\n" , Pot_X_Val , Payload.servo_angle ) ;
         // send packet to receiver's DATA_PIPE_0 address
         success = RF24.send_packet ( &Payload , sizeof ( Payload ) ) ;
 
         //sleep_ms ( FREEZE ) ;
     }
+
     return 0 ;
 }
