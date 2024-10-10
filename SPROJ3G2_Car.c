9f207c3b5a7c60c86ebb0fcd321354290bd7ea12
Bence Tóth <bencike04201@gmail.com>
2024-10-10 10:18:46 +0200

    Header file management
    Header file management

--- SPROJ3G2_Car.c
+++ SPROJ3G2_Car.c
@@ -4,7 +4,7 @@
  * Campus: Sonderborg
  * File: SPROJ3G2_Car.c
  * Author: Bence Toth
- * Date: 08/10/2024
+ * Date: 10/10/2024
  * Course: BEng in Electronics
  * Semester: 3rd
  * Platform: RP2040
@@ -33,38 +33,6 @@
 uint8_t rx_pipe = 0 ;
 payload_t Payload = { 1 , INIT_ANGLE } ;
 nrf_client_t RF24 ;
-
-// GPIO pin numbers for nRF24L01
-pin_manager_t RF_Pins = { 
-    .sck = 2,   // Serial Clock
-    .copi = 3,  // Master Out - Slave In
-    .cipo = 4,  // Master In - Slave Out
-    .csn = 5,   // Chip Select Not
-    .ce = 6     // Chip Enable
-} ;
-
-nrf_manager_t RF_Config = {
-    // RF Channel 
-    .channel = RF_CHANNEL ,
-
-    // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
-    .address_width = AW_5_BYTES ,
-
-    // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-    .dyn_payloads = DYNPD_DISABLE ,
-
-    // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
-    .data_rate = RF_DR_2MBPS ,
-
-    // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-    .power = RF_PWR_0DBM ,
-
-    // retransmission count: ARC_NONE...ARC_15RT
-    .retr_count = ARC_NONE ,
-
-    // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
-    .retr_delay = ARD_250US 
-} ;
 
 int main ( void ) {
 
