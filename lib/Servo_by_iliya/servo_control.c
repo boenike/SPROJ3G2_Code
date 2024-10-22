ab8abe499a98ff0b03864cc6043ba54e883d1922
Bence Tóth <bencike04201@gmail.com>
2024-10-22 16:16:04 +0200

    Refactored code a bit
    Refactored code a bit

--- lib/Servo_by_iliya/servo_control.c
+++ lib/Servo_by_iliya/servo_control.c
@@ -1,51 +1,40 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
 #include "servo_control.h"
 #include "hardware/pwm.h"
 #include "hardware/gpio.h"
-#include "hardware/adc.h"
 #include "hardware/clocks.h"
+//#include "hardware/adc.h"
 
-void joystick_init(){
-    // Enabling ADC for the joystick
-    stdio_init_all(); 
-    adc_init(); // Setup ADC
-    adc_gpio_init(control_pin); // Set the pin for ADC (26-29)
-    adc_select_input(control_pin-26); // Selecting the input (0-3)
-}
+uint8_t period_ms = 1000 / Servo_freq ;     // In milliseconds
+uint16_t pwm_offset = max_pwm_val / 2 ;     // Offset for the PWM signal
 
-void servo_init() {
+void servo_init( uint8_t Servo_pin ) {
     gpio_set_function(Servo_pin, GPIO_FUNC_PWM);   // Setting GPIO for PWM output
 
     uint slice_num = pwm_gpio_to_slice_num(Servo_pin); // Finding the PWM slice connected to the PWM pin
 
     // Setting PWM frequency
     uint32_t clock_freq = clock_get_hz(clk_sys);  // Clock frequency 
-    uint32_t divider16 = clock_freq / (Servo_freq * 65536);  // Calculate divisor
+    uint32_t divider16 = clock_freq / (Servo_freq * max_pwm_val);  // Calculate divisor
+    uint16_t pwm_level = pwm_offset * ( 1 + (uint16_t)( default_degree/max_degree)) ;
     pwm_set_clkdiv(slice_num, divider16);  // Setting the prescaler
 
-    // Initial position of 90 deg. (1.5 ms pulse width)
-    pwm_set_wrap(slice_num, 65535); 
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 49152); 
+    // Set the initial position
+    pwm_set_wrap(slice_num, max_pwm_val); 
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, pwm_level); 
     
     pwm_set_enabled(slice_num, true); // Enable PWM
 }
 
-void set_servo_angle(float angle) {
-    float pulse_min = 1.0f;   // 1 ms for 0 deg
-    float pulse_max = 2.0f;   // 2 ms for 180 deg
-    float period_ms = 20.0f;  // Period of 20 ms (50 Hz)
+void set_servo_angle(uint8_t angle , uint8_t Servo_pin ) {
 
     // Calculate the pulse width for the given angle
-    float pulse_width = pulse_min + (angle / 180.0f) * (pulse_max - pulse_min);
+    float pulse_width = pulse_min + ((float)angle / max_degree) * (pulse_max - pulse_min);
 
     // Convert pulse width to duty cycle
-    uint32_t duty_cycle = (uint32_t)((pulse_width / period_ms) * 65535);
+    uint32_t duty_cycle = (uint32_t)((pulse_width / period_ms) * max_pwm_val);
 
     uint slice_num = pwm_gpio_to_slice_num(Servo_pin);  
     pwm_set_chan_level(slice_num, PWM_CHAN_A, duty_cycle);  // Set the duty cycle
-}
-
-float degrees(int ADC_value) {
-    // ADC value to servo angle conversion
-    float servo_angle = (max_degree / 4095) * ADC_value - mid_degree;
-    return servo_angle;
 }
--- lib/Servo_by_iliya/servo_control.h
+++ lib/Servo_by_iliya/servo_control.h
@@ -1,20 +1,23 @@
-#ifndef SERVO_CONTROL_H
-#define SERVO_CONTROL_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 #include "pico/stdlib.h"
 
 // Constants for servo and joystick control
-#define Servo_pin 14
-#define Servo_freq 50
-#define min_degree 0.0f 
-#define max_degree 180.0f 
-#define mid_degree 90.0f
-#define control_pin 26
+#define Servo_freq 50    // in Hz
+#define pulse_min 0.5f   // 0.5 ms for 0 deg
+#define pulse_max 2.5f   // 2.5 ms for 180 deg
+#define min_degree 0.0f
+#define max_degree 180.0f
+#define default_degree 90.0f    // default value
+#define max_pwm_val 65535
+#define max_adc_val 4095
 
 // Function declarations
-void joystick_init(void);
-void servo_init(void);
-void set_servo_angle(float angle);
-float degrees(int ADC_value);
+void servo_init ( uint8_t Servo_pin ) ;
+void set_servo_angle ( uint8_t angle , uint8_t Servo_pin ) ;
 
-#endif // SERVO_CONTROL_H
+#ifdef __cplusplus
+}
+#endif
