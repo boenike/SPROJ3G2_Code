a4117e08a1997f226d3b4aab948f2ee7161525c9
Bence Tóth <bencike04201@gmail.com>
2024-10-29 18:39:15 +0100

    Implemented safety stop for the car
    Implemented safety stop for the car

--- SPROJ3G2_Car.c
+++ SPROJ3G2_Car.c
@@ -4,7 +4,7 @@
  * Campus: Sonderborg
  * File: SPROJ3G2_Car.c
  * Authors: Bence Toth and Iliya Iliev
- * Date: 28/10/2024
+ * Date: 29/10/2024
  * Course: BEng in Electronics
  * Semester: 3rd
  * Platform: RP2040
@@ -20,6 +20,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 #include "hardware/spi.h"
@@ -30,16 +31,19 @@
 #include "functions.h"
 #include "servo_control.h"
 #include "hardware/i2c.h"
-#include "ssd1306.h"
+//#include "ssd1306.h"
 //#include "hardware/timer.h"
 //#include "hardware/uart.h"
 //#include "hardware/irq.h"
 //#include "pico/multicore.h"
 
 payload_t Payload = { .direction = 1 , .servo_angle = INIT_ANGLE } ;
-echo_t Echo = { .echo = 0 } ;
+//echo_t Echo = { .echo = 0 } ;
 
-//ssd1306_t OLED ;
+//uint16_t rec_packets = 0 ;
+uint8_t car_stopped = 0 ;
+uint32_t currentTime , prevTime = 0 ;
+
 nrf_client_t RF24 ;
 fn_status_t success ;    // Result of packet transmission
 
@@ -49,20 +53,52 @@ int main ( void ) {
 
     gpio_init ( PICO_DEFAULT_LED_PIN ) ;
     gpio_set_dir ( PICO_DEFAULT_LED_PIN , GPIO_OUT ) ;
+    gpio_put ( PICO_DEFAULT_LED_PIN , 0 ) ;
 
     servo_init ( SERVO_PIN ) ;
-
-    //OLED_Setup ( &OLED_Pins , &OLED ) ;
 
     nRF24_Setup ( &RF24 , &RF_Pins , &RF_Config , SPI_BAUDRATE , DYNPD_DISABLE ) ;
     nRF24_Comm_Dir_Setup ( &RF24 , RF24_RX , sizeof ( payload_t ) , sizeof ( echo_t ) , ( data_pipe_t ) payload_pipe , ( data_pipe_t ) echo_pipe , PAYLOAD_ADDRESS , ECHO_ADDRESS ) ;
 
     while ( 1 ) {
+        
+        currentTime = to_ms_since_boot ( get_absolute_time ( ) ) ;
+
+        // If the time delay between two consecutive data packets is greater than the predetermined value, set the car into initial conditions
+        if ( ( currentTime - prevTime ) > INTERVAL_LIMIT && !car_stopped ) {
+            set_servo_angle(INIT_ANGLE , SERVO_PIN ) ;
+            gpio_put ( PICO_DEFAULT_LED_PIN , 1 ) ;
+            car_stopped = 1 ;
+        }
+
         if ( RF24.is_packet ( &payload_pipe ) ) {
             RF24.read_packet ( &Payload , sizeof ( payload_t ) ) ;
-
+            prevTime = to_ms_since_boot ( get_absolute_time ( ) ) ;
+            car_stopped = 0 ;
             gpio_put ( PICO_DEFAULT_LED_PIN , Payload.direction ) ;
             set_servo_angle ( Payload.servo_angle , SERVO_PIN ) ;
+            //printf("recieved a package\n");
+
+            /*
+            rec_packets++ ;
+            if ( rec_packets == RET_LIMIT ) {
+                rec_packets = 0 ;
+                Echo.echo = ( Echo.echo ) ? 0 : 1 ;     // toggle LED
+
+                // send packet to receiver's echo address
+                //RF24.tx_destination ( ECHO_ADDRESS ) ;
+                //RF24.dyn_payloads_disable ( ) ;
+                success = RF24.standby_mode ( ) ;
+                if ( success == NRF_MNGR_OK ) printf ( "Switched to TX mode\n" ) ;
+                //printf("Switched to TX\n");        // Change to transmit mode for echo transmission
+
+                success = RF24.send_packet ( &Echo , sizeof ( echo_t ) ) ;
+                if ( success == NRF_MNGR_OK ) printf ( "Echo sent - %d\n" , Echo.echo ) ;
+
+                success = RF24.receiver_mode ( ) ;        // Change back to receiver mode for normal operation 
+                if ( success == NRF_MNGR_OK ) printf("Switched back to RX\n");
+                printf ( "End of echo.\n\n" ) ;
+            }*/
         }
     }
     return 0 ;
--- SPROJ3G2_Controller.c
+++ SPROJ3G2_Controller.c
@@ -4,7 +4,7 @@
  * Campus: Sonderborg
  * File: SPROJ3G2_Controller.c
  * Authors: Bence Toth and Iliya Iliev
- * Date: 28/10/2024
+ * Date: 29/10/2024
  * Course: BEng in Electronics
  * Semester: 3rd
  * Platform: RP2040
@@ -36,9 +36,9 @@ uint8_t ADC_Pins [ ] = { 26 , 27 } ;
 uint16_t Pot_X_Val ;
 
 payload_t Payload = { .direction = 1 , .servo_angle = INIT_ANGLE } ;
-echo_t Echo = { .echo = 0 } ;
+//echo_t Echo = { .echo = 0 } ;
 
-//ssd1306_t OLED ;
+ssd1306_t OLED ;
 nrf_client_t RF24 ;
 fn_status_t success ;    // Result of packet transmission
 
@@ -46,14 +46,35 @@ const uint16_t THRESHOLD  = ADC_MAX / 2 ;
 const uint16_t LOW_LIMIT  = THRESHOLD - TOLERANCE ;
 const uint16_t HIGH_LIMIT = THRESHOLD + TOLERANCE ;
 
+/*void draw_Bracket ( void ) {
+    ssd1306_draw_line ( &OLED , 0 , 1 , OLED_WIDTH - 1 , 1 ) ;
+    ssd1306_draw_line ( &OLED , 0 , OLED_HEIGHT - 1 , OLED_WIDTH - 1 , OLED_HEIGHT - 1 ) ;
+    ssd1306_draw_line ( &OLED , 0 , 0 , 0 , OLED_HEIGHT - 1 ) ;
+    ssd1306_draw_line ( &OLED , OLED_WIDTH - 1 , 0 , OLED_WIDTH - 1 , OLED_HEIGHT - 1 ) ;
+    ssd1306_show ( &OLED ) ;
+}*/
+
+void draw_Texts ( void ) {
+    ssd1306_draw_string ( &OLED , 0 , 0 , FONT_SCALE , "Car:" ) ;
+    ssd1306_draw_string ( &OLED , 0 , 23 , FONT_SCALE , "Bat:" ) ;
+    ssd1306_draw_string ( &OLED , 0 , 43 , FONT_SCALE , "Status:" ) ;
+    ssd1306_show ( &OLED ) ;
+}
+
 int main ( void ) {
 
     stdio_init_all ( ) ;
 
+    /*gpio_init ( TEST_LED ) ;
+    gpio_set_dir ( TEST_LED , GPIO_OUT ) ;
+    gpio_put (TEST_LED , Echo.echo ) ;*/
+
     uint8_t adc_setup = ADC_Setup ( ADC_Pins , sizeof ( ADC_Pins ) ) ;
     hard_assert ( adc_setup == 1 ) ;
 
-    //OLED_Setup ( &OLED_Pins , &OLED ) ;
+    OLED_Setup ( &OLED_Pins , &OLED ) ;
+    //draw_Bracket ( ) ;
+    draw_Texts ( ) ;
 
     nRF24_Setup ( &RF24 , &RF_Pins , &RF_Config , SPI_BAUDRATE , DYNPD_DISABLE ) ;
     nRF24_Comm_Dir_Setup ( &RF24 , RF24_TX , sizeof ( payload_t ) , sizeof ( echo_t ) , ( data_pipe_t ) payload_pipe , ( data_pipe_t ) echo_pipe , PAYLOAD_ADDRESS , ECHO_ADDRESS ) ;
@@ -73,6 +94,16 @@ int main ( void ) {
 
         // send packet to receiver's payload address
         success = RF24.send_packet ( &Payload , sizeof ( Payload ) ) ;
+        
+        /*
+        RF24.receiver_mode ( ) ;        // Change to receiver mode for echo catch
+        if ( RF24.is_packet ( &echo_pipe ) ) {
+            RF24.read_packet ( &Echo , sizeof ( echo_t ) ) ;
+            gpio_put (TEST_LED , Echo.echo ) ;
+            printf ( "ECHO received\n\n" ) ;
+        }
+        RF24.standby_mode ( ) ;         // Change to transmit mode for normal operation
+        */
     }
     return 0 ;
 }
