5b2846955d255702656ea087d6540e6a09fbe571
Bence Tóth <bencike04201@gmail.com>
2024-11-01 18:38:05 +0100

    Implemented PRX detection at PTX and safety stop of PRX
    Implemented PRX detection at PTX and safety stop of PRX

--- lib/functions/functions.c
+++ lib/functions/functions.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
@@ -6,45 +8,46 @@
 #include "ssd1306.h"
 #include "functions.h"
 
-const uint8_t *PAYLOAD_ADDRESS = ( const uint8_t [ ] ) { 0x37 , 0x37 , 0x37 , 0x37 , 0x37 } ;    // Selected address
+// Address of Data Pipe 0 - based on the datasheet of nRF24L01
+const uint8_t *PAYLOAD_ADDRESS = ( const uint8_t [ ] ) { 0xE7 , 0xD3 , 0xF0 , 0x35 , 0x77 } ;
 uint8_t payload_pipe = 0 ;  // Selected payload pipe
 
-//GPIO pin numbers for the SSD1306 OLED module
+//GPIO pin numbers for I2C - SSD1306 OLED display module
 oled_pins_t OLED_Pins = {
-    .OLED_SDA = 8 ,
-    .OLED_SCL = 9
+    .SDA = 8 ,  // Serial Data
+    .SCL = 9    // Serial Clock
 } ;
 
-// GPIO pin numbers for nRF24L01
+// GPIO pin numbers for SPI - nRF24L01 RF module
 pin_manager_t RF_Pins = { 
-    .sck = 2,   // Serial Clock
-    .copi = 3,  // Master Out - Slave In
-    .cipo = 4,  // Master In - Slave Out
-    .csn = 5,   // Chip Select Not
-    .ce = 6     // Chip Enable
+    .sck  = 2 ,  // Serial Clock
+    .copi = 3 ,  // Master Out - Slave In
+    .cipo = 4 ,  // Master In - Slave Out
+    .csn  = 5 ,  // Chip Select Not
+    .ce   = 6    // Chip Enable
 } ;
 
 nrf_manager_t RF_Config = {
-    // RF Channel 
+    // RF Channel in the ISM band
     .channel = RF_CHANNEL ,
 
-    // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
+    // Address Width: AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
     .address_width = AW_5_BYTES ,
 
-    // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-    .dyn_payloads = DYNPD_ENABLE ,
+    // Dynamic Payloads: DYNPD_ENABLE, DYNPD_DISABLE
+    .dyn_payloads = DYNPD_DISABLE ,
 
-    // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
+    // Air Data Rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
     .data_rate = RF_DR_2MBPS ,
 
-    // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-    .power = RF_PWR_0DBM ,
+    // Output Power: RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
+    .power = RF_PWR_NEG_12DBM ,
 
-    // retransmission count: ARC_NONE...ARC_15RT
-    .retr_count = ARC_1RT ,
+    // Retransmission Count: ARC_NONE...ARC_15RT
+    .retr_count = ARC_2RT ,
 
-    // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
-    .retr_delay = ARD_500US 
+    // Retransmission Delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
+    .retr_delay = ARD_500US
 } ;
 
 int32_t convertInterval ( int32_t x , int32_t in_min , int32_t in_max , int32_t out_min , int32_t out_max ) {
@@ -52,16 +55,17 @@ int32_t convertInterval ( int32_t x , int32_t in_min , int32_t in_max , int32_t 
 }
 
 uint8_t ADC_Setup ( uint8_t *input_pins , uint8_t input_length ) {
-    uint8_t ctr , success = 0 ;
+    uint8_t success = 0 ;
     adc_init ( ) ;
 
     // Make sure GPIO is high-impedance, no pullups etc...
-    for ( ctr = 0 ; ctr < input_length ; ctr++ ) {
+    for ( uint8_t ctr = 0 ; ctr < input_length ; ctr++ ) {
         if ( input_pins [ ctr ] >= ADC_REF_PIN && input_pins [ ctr ] <= ADC_MAX_PIN ) {
             adc_gpio_init ( input_pins [ ctr ] ) ;
             success++ ;
         }
     }
+
     return ( success == input_length ) ? 1 : 0 ;
 }
 
@@ -70,52 +74,87 @@ uint16_t read_ADC ( uint8_t pin ) {
     return adc_read ( ) ;
 }
 
-void nRF24_Setup ( nrf_client_t *RF24_ptr , pin_manager_t *RF24_pins_ptr , nrf_manager_t *RF24_config_ptr , uint32_t baudrate_SPI ,
-            Auto_Ack_t ack_mode , RF_Mode_t mode , size_t payload_size , data_pipe_t payload_pipe , const uint8_t *payload_address )
-{             
-    nrf_driver_create_client ( RF24_ptr ) ;
-    RF24_ptr->configure ( RF24_pins_ptr , baudrate_SPI ) ;      // Set up the correct pinout and set the SPI baudrate
-    RF24_ptr->initialise ( RF24_config_ptr ) ;                  // Initialize the nRF24 module with the stated configuration
-    RF24_ptr->rf_data_rate ( RF24_config_ptr->data_rate ) ;                   // Set the specified air transfer rate
-    RF24_ptr->rf_power ( RF24_config_ptr->power ) ;                           // Set the specified antenna power
-    RF24_ptr->payload_size ( payload_pipe , payload_size ) ;     // Set the size of the payload message
+uint16_t nRF24_Setup ( nrf_client_t *RF24_ptr , pin_manager_t *RF24_pins_ptr , nrf_manager_t *RF24_config_ptr ,
+    Auto_Ack_t ack_mode , RF_Mode_t mode , size_t payload_size , data_pipe_t payload_pipe , const uint8_t *payload_address )
+{
+    uint8_t requirement = 9 + ( uint8_t ) ack_mode ;
+    uint16_t ret = 0 ;
+    fn_status_t state ;
+    retr_count_t ARC_count ;
+
+    state = nrf_driver_create_client ( RF24_ptr ) ;
+    if ( state == NRF_MNGR_OK ) { ret |= 1 << 0 ; }
+
+    state = RF24_ptr->configure ( RF24_pins_ptr , SPI_BAUDRATE ) ;      // Set up the correct pinout and set the SPI baudrate
+    if ( state == PIN_MNGR_OK ) { ret |= 1 << 1 ; }
+
+    state = RF24_ptr->initialise ( RF24_config_ptr ) ;                  // Initialize the nRF24 module with the stated configuration
+    if ( state == SPI_MNGR_OK ) { ret |= 1 << 2 ; }
+
+    state = RF24_ptr->rf_data_rate ( RF24_config_ptr->data_rate ) ;                   // Set the specified air transfer rate
+    if ( state == SPI_MNGR_OK ) { ret |= 1 << 3 ; }
+
+    state = RF24_ptr->rf_power ( RF24_config_ptr->power ) ;                           // Set the specified antenna power
+    if ( state == SPI_MNGR_OK ) { ret |= 1 << 4 ; }
+
+    state = RF24_ptr->payload_size ( payload_pipe , payload_size ) ;     // Set the size of the payload message
+    if ( state == SPI_MNGR_OK ) { ret |= 1 << 5 ; }
 
     switch ( mode ) {
-        case RF24_RX :
-            RF24_ptr->rx_destination ( payload_pipe , payload_address ) ;
-            RF24_ptr->receiver_mode ( ) ;
+        case PRX :
+            state = RF24_ptr->rx_destination ( payload_pipe , payload_address ) ;
+            if ( state == SPI_MNGR_OK ) { ret |= 1 << 6 ; }
+            state = RF24_ptr->receiver_mode ( ) ;
+            if ( state == SPI_MNGR_OK ) { ret |= 1 << 7 ; }
             break ;
-
-        case RF24_TX :
-            RF24_ptr->tx_destination ( payload_address ) ;
-            RF24_ptr->standby_mode ( ) ;
+        case PTX :
+            state = RF24_ptr->tx_destination ( payload_address ) ;
+            if ( state == SPI_MNGR_OK ) { ret |= 1 << 6 ; }
+            state = RF24_ptr->standby_mode ( ) ;
+            if ( state == NRF_MNGR_OK ) { ret |= 1 << 7 ; }
             break ;
-
         default : break ;
     }
-    
-    // Enable Auto-ACK with the specified Retransmission delay and count
-    if ( ack_mode == ACK_ON ) { RF24_ptr->auto_retransmission ( RF24_config_ptr->retr_delay , RF24_config_ptr->retr_count ) ; }
 
     switch ( RF24_config_ptr->dyn_payloads ) {
         case DYNPD_ENABLE :
-            RF24_ptr->dyn_payloads_enable ( ) ;
+            state = RF24_ptr->dyn_payloads_enable ( ) ;
+            if ( state == SPI_MNGR_OK ) { ret |= 1 << 8 ; }
             break ;
 
         case DYNPD_DISABLE :
-            RF24_ptr->dyn_payloads_disable ( ) ;
+            state = RF24_ptr->dyn_payloads_disable ( ) ;
+            if ( state == NRF_MNGR_OK ) { ret |= 1 << 8 ; }
             break ;
         default : break ;
     }
+
+    switch ( ack_mode ) {
+        case ACK_OFF :
+            ARC_count = ARC_NONE ;
+            break ;
+        case ACK_ON :
+            ARC_count = RF24_config_ptr->retr_count ;
+            break ;
+        default :
+            ARC_count = ARC_NONE ;
+            break ;
+    }
+
+    // Enable Auto-ACK with the specified Retransmission delay and count
+    state = RF24_ptr->auto_retransmission ( RF24_config_ptr->retr_delay , ARC_count ) ;
+    if ( state == SPI_MNGR_OK ) { ret |= 1 << 9 ; }
+
+    return ret ;
 }
 
 void set_Payload_Data ( payload_t *payload , uint8_t pot_x_pin ) {
-    uint16_t Pot_X_Val ;
+    uint16_t Pot_X_Val , Pot_Y_Val ;
     const uint16_t THRESHOLD  = ADC_MAX / 2 ;
     const uint16_t LOW_LIMIT  = THRESHOLD - TOLERANCE ;
     const uint16_t HIGH_LIMIT = THRESHOLD + TOLERANCE ;
 
-    Pot_X_Val = read_ADC ( POT_X_PIN ) ;
+    Pot_X_Val = read_ADC ( pot_x_pin ) ;
 
     if ( Pot_X_Val >= HIGH_LIMIT || Pot_X_Val <= LOW_LIMIT ) {
         payload->servo_angle = ( uint8_t ) convertInterval ( ( int32_t ) Pot_X_Val , ADC_MIN , ADC_MAX , MIN_ANGLE , MAX_ANGLE ) ;
@@ -128,33 +167,39 @@ void set_Payload_Data ( payload_t *payload , uint8_t pot_x_pin ) {
     }
 }
 
-void OLED_Setup ( oled_pins_t *oled_pins , ssd1306_t *oled_ptr ) {
+uint8_t OLED_Setup ( oled_pins_t *oled_pins , ssd1306_t *oled_ptr ) {
+    uint8_t setup_success = 0 ;
     i2c_inst_t *current_i2c_instance ;
 
-    if ( oled_pins->OLED_SDA % 4 == 0 ) current_i2c_instance = i2c0 ;
+    if ( oled_pins->SDA % 4 == 0 ) current_i2c_instance = i2c0 ;
     else current_i2c_instance = i2c1 ;
 
-    i2c_init ( current_i2c_instance , I2C_BAUDRATE ) ;
+    uint rate = i2c_init ( current_i2c_instance , I2C_BAUDRATE ) ;
 
-    gpio_set_function ( oled_pins->OLED_SDA , GPIO_FUNC_I2C ) ;
-    gpio_set_function ( oled_pins->OLED_SCL , GPIO_FUNC_I2C ) ;
-    gpio_pull_up ( oled_pins->OLED_SDA ) ;
-    gpio_pull_up ( oled_pins->OLED_SCL ) ;
+    gpio_set_function ( oled_pins->SDA , GPIO_FUNC_I2C ) ;
+    gpio_set_function ( oled_pins->SCL , GPIO_FUNC_I2C ) ;
+    gpio_pull_up ( oled_pins->SDA ) ;
+    gpio_pull_up ( oled_pins->SCL ) ;
 
     oled_ptr->external_vcc = false ;
-    ssd1306_init ( oled_ptr , OLED_WIDTH , OLED_HEIGHT , OLED_ADDRESS , current_i2c_instance ) ;
+    setup_success += ( uint8_t ) ssd1306_init ( oled_ptr , OLED_WIDTH , OLED_HEIGHT , OLED_ADDRESS , current_i2c_instance ) ;
+    ssd1306_poweron ( oled_ptr ) ;
     ssd1306_clear ( oled_ptr ) ;
     ssd1306_show ( oled_ptr ) ;
+    
+    return setup_success ;
 }
 
 void draw_Initial_Texts ( ssd1306_t *oled_ptr ) {
     ssd1306_draw_string ( oled_ptr , 0 , 0 , FONT_SCALE , "Car:" ) ;
-    ssd1306_draw_string ( oled_ptr , 0 , 23 , FONT_SCALE , "Bat:" ) ;
-    //ssd1306_draw_string ( oled_ptr , 0 , 43 , FONT_SCALE , "Status:" ) ;
+    ssd1306_draw_string ( oled_ptr , 0 , 28 , FONT_SCALE , "Bat:" ) ;
     ssd1306_show ( oled_ptr ) ;
 }
 
-void draw_Car_Status ( ssd1306_t *oled_ptr , uint8_t status ) {
-    ssd1306_draw_string ( oled_ptr , 60 , 0 , FONT_SCALE , ( status ) ? "ON  " : "OFF " ) ;
+void update_Car_Status ( ssd1306_t *oled_ptr , uint8_t status ) {
+    ssd1306_clear ( oled_ptr ) ;
+    ssd1306_draw_string ( oled_ptr , 0 , 0 , FONT_SCALE , "Car:" ) ;
+    ssd1306_draw_string ( oled_ptr , 0 , 28 , FONT_SCALE , "Bat:" ) ;
+    ssd1306_draw_string ( oled_ptr , 60 , 0 , FONT_SCALE , ( status ) ? "ON" : "OFF" ) ;
     ssd1306_show ( oled_ptr ) ;
 }
--- lib/functions/functions.h
+++ lib/functions/functions.h
@@ -8,26 +8,30 @@
 #include "nrf24_driver.h"
 #include "ssd1306.h"
 
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
-#define OLED_ADDRESS 0x3C
 #define SERVO_PIN  8
 #define ADC_REF_PIN 26
 #define ADC_MAX_PIN 29
 #define POT_X_PIN 26
 #define POT_Y_PIN 27
+
 #define INIT_ANGLE 90
 #define MAX_ANGLE 180
 #define MIN_ANGLE 0
 #define ADC_MIN 0
 #define ADC_MAX 4095
-#define TOLERANCE 50
-#define SPI_BAUDRATE 4000000    // 4 MHz SPI baudrate
+#define TOLERANCE 40
+
+#define SPI_BAUDRATE 4000000            // 4 MHz SPI baudrate
+#define RF_CHANNEL 50                   // ISM frequency band
+#define INTERVAL_LIMIT 50               // Defines the maximum time interval between consecutive received data packets for the Car
+#define MAX_RT_TRY 15                   // Defines the maximum Retransmission count of the Controller
+#define RF_SETUP_OK 0b0000001111111111  // Checks if all RF setup functions returned the correct values
+
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+#define OLED_ADDRESS 0x3C
 #define I2C_BAUDRATE 400000     // 400 kHz I2C baudrate
-#define RF_CHANNEL 110          // 2.51 GHz ISM frequency band
 #define FONT_SCALE 2
-#define INTERVAL_LIMIT 200       // Defines the maximum time interval between consecutive received data packets for the Car
-#define MAX_RT_TRY 40          // Defines the maximum Retransmission count of the Controller
 
 typedef struct {                // The payload structure for transmission
     uint8_t direction ;
@@ -35,21 +39,20 @@ typedef struct {                // The payload structure for transmission
 } payload_t ;
 
 typedef struct {
-    uint8_t OLED_SDA ;
-    uint8_t OLED_SCL ;
+    uint8_t SDA ;
+    uint8_t SCL ;
 } oled_pins_t ;
 
 extern oled_pins_t OLED_Pins ;
+
 extern pin_manager_t RF_Pins ;
 extern nrf_manager_t RF_Config ;
 
 extern const uint8_t *PAYLOAD_ADDRESS ;
-extern const uint8_t *ECHO_ADDRESS ;
-
 extern uint8_t payload_pipe ;
 
-typedef enum { RF24_TX = 0 , RF24_RX = 1 } RF_Mode_t ;    // Determines whether the module is primary transmitter or primary receiver
-typedef enum { ACK_OFF = 0 , ACK_ON = 1 } Auto_Ack_t ;
+typedef enum { PTX = 0 , PRX = 1 } RF_Mode_t ;              // Determines whether the module is primary transmitter or primary receiver
+typedef enum { ACK_OFF = 0 , ACK_ON = 1 } Auto_Ack_t ;      // Determines whether to enable or disable Auto-ACK
 
 int32_t convertInterval ( int32_t x , int32_t in_min , int32_t in_max , int32_t out_min , int32_t out_max ) ;
 
@@ -57,16 +60,16 @@ uint8_t ADC_Setup ( uint8_t *input_pins , uint8_t input_length ) ;
 
 uint16_t read_ADC ( uint8_t pin ) ;
 
-void nRF24_Setup ( nrf_client_t *RF24_ptr , pin_manager_t *RF24_pins_ptr , nrf_manager_t *RF24_config_ptr , uint32_t baudrate_SPI ,
-            Auto_Ack_t ack_mode , RF_Mode_t mode , size_t payload_size , data_pipe_t payload_pipe , const uint8_t *payload_address ) ;
+uint16_t nRF24_Setup ( nrf_client_t *RF24_ptr , pin_manager_t *RF24_pins_ptr , nrf_manager_t *RF24_config_ptr ,
+    Auto_Ack_t ack_mode , RF_Mode_t mode , size_t payload_size , data_pipe_t payload_pipe , const uint8_t *payload_address ) ;
 
 void set_Payload_Data ( payload_t *payload , uint8_t pot_x_pin ) ;
 
-void OLED_Setup ( oled_pins_t *oled_pins , ssd1306_t *oled_ptr ) ;
+uint8_t OLED_Setup ( oled_pins_t *oled_pins , ssd1306_t *oled_ptr ) ;
 
 void draw_Initial_Texts ( ssd1306_t *oled_ptr ) ;
 
-void draw_Car_Status ( ssd1306_t *oled_ptr , uint8_t status ) ;
+void update_Car_Status ( ssd1306_t *oled_ptr , uint8_t status ) ;
 
 #ifdef __cplusplus
 }
